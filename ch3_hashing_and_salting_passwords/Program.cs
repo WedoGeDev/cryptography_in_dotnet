78b1152a68012000927d996fad29d165b475368f
Omar García Espíritu <wedoge@MacBook-Pro-de-Omar.local>
2023-03-20 16:37:02 -0600

    hashing and salting passwords chapter added

---
+++ ch3_hashing_and_salting_passwords/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+namespace ch3_hashing_and_salting_passwords;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var name = "Omar";
+
+        var encryptedName = EncryptData(name);
+
+        Console.WriteLine(encryptedName);
+
+        if (ValidateEncryptedData(name, encryptedName))
+        {
+            Console.WriteLine($"The {nameof(name)} entered is correct.");
+        }
+        else
+        {
+            Console.WriteLine($"The {nameof(name)} entered is not correct.");
+        }
+    }
+
+    private static bool ValidateEncryptedData(string valueToValidate, string valueFromDataBase)
+    {
+        var arrValues = valueFromDataBase.Split(':');
+        var encryptedValue = arrValues[0];
+        var salt = arrValues[1];
+        var saltedValue = Encoding.UTF8.GetBytes(salt + valueToValidate);
+        var hash = SHA256.HashData(saltedValue);
+        var enteredValueToValidate = Convert.ToBase64String(hash);
+
+        return encryptedValue.Equals(enteredValueToValidate);
+    }
+
+    private static string EncryptData(string valueToEncrypt)
+    {
+        string GenerateSalt()
+        {
+            var salt = new byte[32];
+            var generator = RandomNumberGenerator.Create();
+            generator.GetBytes(salt);
+
+            return Convert.ToBase64String(salt);
+        }
+
+        string EncryptValue(string strValue)
+        {
+            var saltValue = GenerateSalt();
+            var saltedPassword = Encoding.UTF8.GetBytes(saltValue + strValue);
+
+            var hash = SHA256.HashData(saltedPassword);
+
+            return $"{Convert.ToBase64String(hash)}:{saltValue}";
+        }
+
+        return EncryptValue(valueToEncrypt);
+    }
+}
